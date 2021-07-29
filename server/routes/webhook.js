74b5c8403e5d26e58419b1c44572b96ba6b060d5
bouyo madaliou <bouyomadaliou@gmail.com>
2021-07-29 16:56:37 +0000

    heroku

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -17,11 +17,11 @@ const bankAPI = "https://bank70.herokuapp.com/api";
 
 router.post('/', (request, response) => {
 
-    console.log('enter');
+    //console.log('enter');
     const _agent = new WebhookClient({ request, response });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  /*   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
 
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body)); */
 
     function backend(agent) {
       agent.add(`Test du 23-07-2021, Welcome to my agent from madaliou express!`);
@@ -32,7 +32,7 @@ router.post('/', (request, response) => {
     }
 
     const transfert = async ( agent) => {
-      console.log('request.body.queryResult.parameters', request.body.queryResult.parameters);
+      //console.log('request.body.queryResult.parameters', request.body.queryResult.parameters);
       //const {receiver_account_number, sender_account_number, amount, ext_bank_name} = request.body.queryResult.parameters;
       const receiver_account_number = request.body.queryResult.parameters.toAccount;
       const sender_account_number = request.body.queryResult.parameters.fromAccount;
@@ -51,9 +51,9 @@ router.post('/', (request, response) => {
                 agent.add('The Transfert is Done successfully');
             })
             .catch(err => {
-                console.log('pas cool : ', err);
+                console.log('pas cool : ', err.response);
                 
-                agent.add('An error occured');
+                agent.add( err.response.data.message ? `${err.response.data.message}` : 'An error occured!!');
             });
      
     }
@@ -71,20 +71,47 @@ router.post('/', (request, response) => {
                 /* if (resp.status === 200) {
                     result.data = resp.data;
                 } */
-                agent.add('Your account balance is Done', resp.data);
+                agent.add(`Your account balance is ${resp.data.sold}`);
             })
             .catch(err => {
                 console.log('pas cool : ', err);
                 
-                agent.add('Error');
+                agent.add( err.response.data.message ? `${err.response.data.message}` : 'An error occured!!');
             });
      
     }
+
+    const credit_account = async ( agent) => {
+       
+        //const {receiver_account_number, sender_account_number, amount, ext_bank_name} = request.body.queryResult.parameters;
+        const {account_number, amount} = request.body.queryResult.parameters;
+        
+          
+  
+        await axios.post(`${bankAPI}/account/credit`, {account_number, amount})
+              .then(resp => {
+                  console.log('cool : ', resp.data);
+                  /* if (resp.status === 200) {
+                      result.data = resp.data;
+                  } */
+                  agent.add(`The credit is done successfully!!`);
+              })
+              .catch(err => {
+                  console.log('pas cool : ', err.response);
+
+                  agent.add( err.response.data.message ? `${err.response.data.message}` : 'An error occured!!');
+                  
+              });
+       
+      }
+      
     
     let intents = new Map();
     intents.set('TestBackend', backend);
     intents.set('chatbank', chatbank);
     intents.set('transfert', transfert);
+    intents.set('get-my-solde', balance);
+    intents.set('credit-account', credit_account);    
 
     _agent.handleRequest(intents);
 });
